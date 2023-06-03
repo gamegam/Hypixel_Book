4ef59c3fe7394086da1928c5f26c5f117a8dc711
플그러 <48847058+gamegam@users.noreply.github.com>
2023-06-03 11:01:41 +0900

    Add files via upload

---
+++ src/gamegam/HypixelBook/Task/Time.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace gamegam\HypixelBook\Task;
+
+use pocketmine\scheduler\Task;
+use pocketmine\player\Player;
+use pocketmine\Server;
+
+class Time extends Task{
+
+    private $p;
+
+    public function __construct(Player $p){
+        $this->p = $p;
+    }
+
+    public function onRun():void{
+        $api = Server::getInstance()->getPluginManager()->getPlugin("HypixelBook");
+        $name = $this->p->getName();
+        if ($this->p instanceof Player && $this->p->isOnline()){
+            if (isset($this->db["기본"][$name])){
+                $api->Book($this->p);
+            }
+            if (isset($api->db[$this->p->getName()])){
+                $this->p->getInventory()->setHeldItemIndex($api->config->get("slot"));
+            }
+        }
+    }
+}
